a77dfb658c6f31b1806dea9614a849f123e626ed
Adnan <68817284+AdnanH1006@users.noreply.github.com>
2022-03-26 16:31:46 +0600

    Promise.race()

--- app.js
+++ app.js
@@ -25,35 +25,47 @@ const setTimer = (duration) => {
   return promise;
 };
 
-async function trackUserHandler() {
-  //let positionData;
-  let posData;
-  let timerData;
-  try {
-    posData = await getPosition();
-    timerData = await setTimer(2000);
-  } catch (error) {
-    console.log(error);
-  }
-  console.log(timerData, posData);
-  // .then((posData) => {
-  //   positionData = posData;
-  //   return setTimer(2000);
-  // })
-  // .catch((err) => {
-  //   console.log(err);
-  //   return "on we go...";
-  // })
-  // .then((data) => {
-  //   console.log(data, positionData);
-  // });
-  setTimer(1000).then(() => {
-    console.log("Timer done!");
-  });
-  console.log("Getting position...");
-}
+// async function trackUserHandler() {
+//   //let positionData;
+//   let posData;
+//   let timerData;
+//   try {
+//     posData = await getPosition();
+//     timerData = await setTimer(2000);
+//   } catch (error) {
+//     console.log(error);
+//   }
+//   console.log(timerData, posData);
+// .then((posData) => {
+//   positionData = posData;
+//   return setTimer(2000);
+// })
+// .catch((err) => {
+//   console.log(err);
+//   return "on we go...";
+// })
+// .then((data) => {
+//   console.log(data, positionData);
+// });
+//   setTimer(1000).then(() => {
+//     console.log("Timer done!");
+//   });
+//   console.log("Getting position...");
+// }
 
-button.addEventListener("click", trackUserHandler);
+// button.addEventListener("click", trackUserHandler);
+
+Promise.race([getPosition(), setTimer(1000)]).then((data) => {
+  console.log(data);
+});
+
+// Promise.all([getPosition(), setTimer(1000)]).then((promiseData) => {
+//   console.log(promiseData);
+// });
+
+// Promise.allSettled([getPosition(), setTimer(1000)]).then((promiseData) => {
+//   console.log(promiseData);
+// });
 
 // let result = 0;
 // for (let i = 0; i < 100000000; i++) {
