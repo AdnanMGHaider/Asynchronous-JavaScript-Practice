c722bc784d26754aa5479080a61bd3e4808f0e9f
Adnan <68817284+AdnanH1006@users.noreply.github.com>
2022-03-26 12:58:01 +0600

    Chaining multiple promises | Error Handling | .catch()

--- app.js
+++ app.js
@@ -1,27 +1,44 @@
 const button = document.querySelector("button");
 const output = document.querySelector("p");
 
+const getPosition = (options) => {
+  const promise = new Promise((resolve, reject) => {
+    navigator.geolocation.getCurrentPosition(
+      (success) => {
+        resolve(success);
+      },
+      (error) => {
+        reject(error);
+      },
+      options
+    );
+  });
+  return promise;
+};
+
 const setTimer = (duration) => {
   const promise = new Promise((resolve, reject) => {
     setTimeout(() => {
       resolve("Done!"); //This is not just limited to a string. It can be anything.
     }, duration);
   });
-
   return promise;
 };
 
 function trackUserHandler() {
-  navigator.geolocation.getCurrentPosition(
-    (posData) => {
-      setTimer(2000).then((data) => {
-        console.log(data, posData);
-      });
-    },
-    (error) => {
-      console.log(error);
-    }
-  );
+  let positionData;
+  getPosition()
+    .then((posData) => {
+      positionData = posData;
+      return setTimer(2000);
+    })
+    .catch((err) => {
+      console.log(err);
+      return "on we go...";
+    })
+    .then((data) => {
+      console.log(data, positionData);
+    });
   setTimer(1000).then(() => {
     console.log("Timer done!");
   });
